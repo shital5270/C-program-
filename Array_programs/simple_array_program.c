ad6df558adb240f66b71f7bf4d1fa9e092e3ace9
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-09-25 21:29:07 +0530

    Add files via upload

---
+++ Array_programs/simple_array_program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int value[5]={4,5,45,65,76};
+  
+  printf("\n 1st number is '%d'",value [0]);
+  printf("\n 2nd number is '%d'",value [1]);
+  printf("\n 3rd number is '%d'",value [2]);
+  printf("\n 4th number is '%d'",value [3]);
+  printf("\n 5th number is '%d'",value [4]);
+  
+  getch();
+  return 0;
+ }
