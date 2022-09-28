bba1182b21962ffd476029bdd0b988d89b25ee16
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-09-28 22:39:10 +0530

    Add files via upload

---
+++ Conditional_control_statement/day_if_else_ladder.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int day=0;
+  
+  printf("n enter day number (1-7) to view day text =");
+  scanf("%d",&day);
+  
+  if(day==1)
+  {
+    printf("\n monday");
+  }
+  else if (day==2)
+  {
+    printf("\n tuesday");
+  }
+  else if (day==3)
+  {
+    printf("\n Wednesday");
+  }
+  else if (day==4)
+  {
+    printf("\n thrusday");
+  }
+  else if (day==5)
+  {
+    printf("\n Friday");
+  }
+  else if (day==6)
+  {
+    printf("\n staturday");
+  }
+  else if (day==7)
+  {
+    printf("\n sunday");
+  }
+  else
+  {
+    printf("\n invaild day !!!");
+  }
+  printf("\n\n thanks !!!");
+  
+  getch();
+  return 0;
+ }
