10991aba1f8a23552241a21e724bc34c426fded4
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-09-28 17:34:49 +0530

    Add files via upload

---
+++ Conditional_control_statement/maximum_number_using_if_else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int num1=25,num2=101;
+    
+    if(num1>num2)
+    {
+        printf("\n number %d is maximum",num1);
+    }
+    else
+    { 
+        printf("\n number %d is maximum",num2);
+    }
+    
+    printf("\n thanks");
+    
+    getch();
+    return 0;
+    
+ }
