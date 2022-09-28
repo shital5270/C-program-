f44cb6f9a49f0f946096748f636c974fb09cf886
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-09-28 18:19:12 +0530

    Add files via upload

---
+++ Basic/addition_of_two_number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+ int main()
+ { 
+   int no1=50,no2=25,sum=0;
+   sum=no1+no2;
+   printf("\n addition of %d+%d is = %d",no1,no2,sum);
+   getch();
+   return 0;
+ }
