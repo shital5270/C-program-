5f82cc33302a90b6741db26d727434cb22883016
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-09-28 17:36:47 +0530

    Add files via upload

---
+++ Basic/ascii_table_for_character.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{ 
+    char ch=10;
+    
+    printf("\n enter a character to get ascii value");
+    scanf("%c",&ch);
+    
+    printf("\n\n ascii value for [%c] character is [%d]",ch,ch);
+    
+    getch();
+    return 0;
+ }
