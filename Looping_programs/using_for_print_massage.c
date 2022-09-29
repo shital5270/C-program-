2890bcab1c3b95aefda220216a3d2e324d9f2e6e
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-09-29 10:21:31 +0530

    Add files via upload

---
+++ Looping_programs/using_for_print_massage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+  int i=0;
+  
+  for(i=1;i<=5;i++)
+  {
+    printf("\n\n India");
+    printf("\n I love my India");
+  }
+  printf("\n\n thanks !!!");
+  
+  getch();
+  return 0;
+ }
