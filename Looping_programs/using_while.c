84292f9db3ab3139c3a147a26be26a9c87dde249
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-09-29 10:23:07 +0530

    Add files via upload

---
+++ Looping_programs/using_while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main ()
+{
+  int i=1;
+  while (i<=5)
+  {
+    printf("\n India");
+    printf(";n I have India");
+    printf("\n value of variable =%d \n",i);
+    i++;
+  }
+  printf("\n\n thanks !!!");
+  
+  getch();
+  return 0;
+ }
