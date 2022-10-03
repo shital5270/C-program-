7efed3e67a11c78cc0009884efcdcd4d7e8861fb
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-10-03 23:40:12 +0530

    Add files via upload

---
+++ Conditional_control_statement/odd_even.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include <conio.h>
+int main()
+{
+     int num =0;
+     printf("\nEnter an integer Number =");
+     scanf("%d",&num);
+     if(num % 2 ==0)
+     {
+            printf("\nGiven number is even");
+     }
+     else
+     {
+            printf("\nGiven number is odd");
+     }
+     getch();
+     return 0;
+ }
+     
+     
