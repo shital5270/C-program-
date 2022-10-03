9e2e120e154202ae1a6559fc9c7294e7681fd69d
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-10-03 23:55:11 +0530

    Add files via upload

---
+++ Pattern_printing/pattern_simple_print_massage.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i=0,j=0;
+   printf("\n wellcome \n");
+   for(i=100; i>97; i--)
+   {
+     printf("\n Ganapati bappa \n");
+     
+     for(j=1;j<=5;j++)
+     {
+        printf("\n karad \n");
+     }
+        printf("\n india\n");
+   }
+     printf("\n\n Harry pottery");
+   getch();
+   return 0;
+oo
