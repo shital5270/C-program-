20364a3870a111ba6ef7b3f73bf01ceffb7ce3df
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-10-04 23:01:47 +0530

    Add files via upload

---
+++ Basic/fork_infosystem_n_times_display.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int num=0,cnt=0;
+  
+  printf("\n\t enter positive number for printing name of class=");
+  scanf("%d",&num);
+  
+  printf("\n\n\t ======*****====== \n");
+  
+  for (cnt=1;cnt<=num;cnt++)
+  {
+    printf("\n\t\t FORK INFOSYSTEM \n");
+  }
+  
+  printf("\n\t\t ======*****====== \n");
+  printf("\n\n\t Thanks !!! \n");
+  
+  getch();
+  return 0;
+ }
