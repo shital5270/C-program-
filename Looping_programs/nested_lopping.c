31ae58b557fea0892f4aeb40fb66202363bd32ba
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-10-05 00:54:24 +0530

    Add files via upload

---
+++ Looping_programs/nested_lopping.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+  int i=0,j=0;
+  
+  printf("\n\t Wellcome to class \n");
+  
+  for(i=0;i<=4;i++)
+  {
+    for(j=1;j<=5;j++)
+    {
+      printf("\n\t fork infosystems");
+    }
+    printf("\n\t karad \n");
+  }
+  printf("\n\n\t Thanks !!!");
+ 
+  getch();
+  return 0;
+ }
