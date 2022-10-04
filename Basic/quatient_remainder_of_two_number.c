ee55e8ad3cd1411ca95a0bf6932deba599150970
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-10-04 23:31:20 +0530

    Add files via upload

---
+++ Basic/quatient_remainder_of_two_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int num1=0,num2=0;
+  
+  printf("\n\t enter frist number =");
+  scanf("%d",&num1);
+  
+  printf("\n\t enter second number =");
+  scanf("%d",&num2);
+  
+  printf("\n\t quatient =%d",num1/num2);
+  printf("\n\t remainder =%d",num1%num2);
+  
+  printf("\n\t\t Thanks !!! \n");
+  
+  getch();
+  return 0;
+ }
+    
