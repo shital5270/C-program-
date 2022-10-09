94e5a34633eb0e6f7ad4c236305227efe1c11301
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-10-09 16:16:16 +0530

    Add files via upload

---
+++ String_programs/gets_puts_demo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  char Name[50]={};
+  
+  puts("\n enter a string :");
+  gets(Name);
+  
+  puts("\n given string is ");
+  puts(Name);
+  
+  getch();
+  return 0;
+ }
