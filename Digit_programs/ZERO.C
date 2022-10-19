128cd413db5f467266eefcfe81cf42d6c2105ed8
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-10-19 16:45:14 +0530

    Add files via upload

---
+++ Digit_programs/ZERO.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int No=0,Zcnt=0,Temp=0,Dig=0;
+
+	up:
+	printf("\n\n Enter positive integer number to count of zero=");
+	scanf("%d",&No);
+
+	if(No<=0)
+	{
+		printf("\n Invalid number please enter positive integer number.");
+		goto up;
+	}
+
+	Temp=No;
+
+	while(Temp>0)
+	{
+		Dig=Temp%10;
+
+		if(Dig==0)
+		{
+			Zcnt++;
+		}
+		Temp=Temp/10;
+	}
+	printf("\n\n count of zero in given number %d is %d",No,Zcnt);
+	printf("\n\n Thanks !!! \n ");
+
+	getch();
+	return 0;
+}
