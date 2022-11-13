cab657faabae0e31a1a7019a2cc353579861b2ff
shital5270 <113083497+shital5270@users.noreply.github.com>
2022-11-13 19:14:53 +0530

    Add files via upload

---
+++ Array_programs/Array_Of_6th_Element_Without_Using_loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+    int Arr[7]={'\0'};
+    
+    printf("\n\n Enter 1st Element");
+    scanf("%d",&Arr[0]);
+    printf("\n\n Enter 6th Element");
+    scanf("%d",&Arr[5]);
+    
+    getch();
+   
+    printf("\n\n Enter 1st Element=%d",Arr[0]);
+    printf("\n\n Enter 6th Element=%d",Arr[5]);
+    
+    printf("\n\n Thanks !!!");
+    
+    getch();
+    return 0;
+ }
+    
+ 
