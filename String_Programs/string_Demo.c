ad297f8534399b12833c589b5b6e11d6658e1d8f
shital5270 <113083497+shital5270@users.noreply.github.com>
2023-04-23 21:14:53 +0530

    Add files via upload

---
+++ String_Programs/string_Demo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+{
+    char cArr[20]={'K','A','R','A','D'};
+    
+    puts(cArr);
+    
+    puts("\n Enter a string :");
+    gets(cArr);
+    
+    puts("\n Given string is :");
+    puts(cArr);
+    
+    getch();
+    return 0;
+    
+ }
+    
+    
