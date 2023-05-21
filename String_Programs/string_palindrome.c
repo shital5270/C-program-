36ae04b410c8cc3dbbffdce2d564e532c7c9cb96
shital5270 <113083497+shital5270@users.noreply.github.com>
2023-05-21 13:30:23 +0530

    Add files via upload

---
+++ String_Programs/string_palindrome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+{
+      char cSrc[50] = {'\0'};
+      int i=0,j=0;
+      
+      puts("\n Enter a string ");
+      gets(cSrc);
+      
+      while(cSrc[i] !='\0')
+      {
+            i++;
+      }
+      i=i-1;
+      
+      while(i>j)
+      {
+          if(cSrc[i] != cSrc[j])
+          {
+                break;
+          }
+          j++;
+          i--;
+      }
+      if(i<=j)
+      
+      printf("\n Given String is palindrome !!!",cSrc);
+      
+      else
+
+      printf("\n Given String is not palindrome !!!",cSrc);
+      
+      getch();
+      return 0;
+      
+ }
+      
