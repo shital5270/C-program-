1b35005ab811f6df7179a08426b17ec5cd63337b
shital5270 <113083497+shital5270@users.noreply.github.com>
2023-05-22 23:15:25 +0530

    Add files via upload

---
+++ String_Programs/string_program_inside_main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+{
+      char cArr[50] = {'\0'};
+      
+      puts("\n Enter a string ");
+      gets(cArr);
+      
+      puts("\n Given String is = ");
+      puts(cArr);
+      
+      getch();
+      return 0;
+      
+ }
