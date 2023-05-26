71e73629efd32241ed8aebca5d38030ed98f53ce
shital5270 <113083497+shital5270@users.noreply.github.com>
2023-05-26 14:11:55 +0530

    Add files via upload

---
+++ String_Programs/string_length_in_main2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+{
+      char cSrc[20] = {'\0'};
+      int i=0;
+      
+      puts("\n Enter a string ");
+      gets(cSrc);
+      
+      while(cSrc[i]!= '\0')
+      {
+            i++;
+      }
+           
+      printf("\n Length of given string is = %d",i);
+      
+      getch();
+      return 0;
+      
+ }
+      
